6ffa168cac175b02d0964547789ee0264281c388
heokor3 <musulmaninber@gmail.ru>
2023-04-17 13:52:10 +0600

    first commit

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ from aiogram import Bot, Dispatcher, executor, types
 import os
 load_dotenv()
 BOT_TOKEN = '6244264554:AAEhGtoAClfYP464Fd96NwXDQpDKRqf4t_E'
-bot = Bot(token=os.getenv(BOT_TOKEN))
+bot = Bot(os.getenv('BOT_TOKEN'))
 dp = Dispatcher(bot)
